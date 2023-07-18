437b97cb71328654e9b75953ef09698d14e3aa34
eswaribala <parameswaribala@gmail.com>
2023-07-18 15:53:08 +0530

    updated

---
+++ customerapiexternal/src/main/java/com/boa/customerapiext/configurations/VaultConfiguration.java
@@ -0,0 +1,17 @@
+package com.boa.customerapiext.configurations;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@ConfigurationProperties
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VaultConfiguration {
+
+	private String username;
+	private String password;
+}
